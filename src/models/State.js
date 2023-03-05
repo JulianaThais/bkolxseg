0ccc782ab9d38e3404ca493629ea727af7b31bd4
JulianaThais <julianaveloso611@gmail.com>
2023-03-05 04:01:57 -0300

    finalizando B.E

--- src/models/State.js
+++ src/models/State.js
@@ -3,12 +3,8 @@ mongoose.Promise = global.Promise;
 
 const modelSchema = new mongoose.Schema({
 	name: String,
-<<<<<<< HEAD
-	pais: String
-=======
     contry: String
 
->>>>>>> controllers
 });
 
 const modelName = 'State';
@@ -17,8 +13,4 @@ if (mongoose.connection && mongoose.connection.models[modelName]) {
 	module.exports = mongoose.connection.models[modelName];
 } else {
 	module.exports = mongoose.model(modelName, modelSchema);
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> controllers
